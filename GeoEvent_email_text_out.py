406125e695fbce4ad2a0809d4db69b84f9874a98
Jason Matney <jamatney@gmail.com>
2016-10-10 12:22:42 -0400

    Create GeoEvent_email_text_out.py

---
+++ GeoEvent_email_text_out.py
@@ -0,0 +1,31 @@
+<img src="https://upload.wikimedia.org/wikipedia/en/thumb/6/6e/Esri_logo.svg/1280px-Esri_logo.svg.png" alt='xxx' width='80'/>
+<h1 style='font-family:Segoe, Arial, sans-serif;font-weight: 700;'>${Project_Name}</h1>
+<table style='font-family:Segoe, Arial, sans-serif;font-weight: 400;border-collapse:collapse;'cellpadding='5'>
+<tr style='background-color:lightgrey;'>
+ <td style='font-weight:600;' width='50px'>OBJECTID:</td>
+ <td width='150px'>${OBJECTID}</td>
+ </td>
+</tr>
+<tr>
+ <td colspan='5'>
+ <table style='width:100%; border-bottom-style:solid; border-bottom-width:3px; border-right-style:solid; border-right-width:3px; border-bottom-color:#888888; border-right-color:#888888;'>
+ <tr style='font-weight:700; font-size:larger; background-color:lightgrey'>
+ <td colspan='5'>Applicant Information</td>
+ </tr>
+<tr>
+ <td style='font-weight:600;' width='130px'>Applicant Name:</td>
+ <td width='160px'>${ApplicantContactName}</td>
+ <td width='50px'>
+ </td>
+ <td style='font-weight:600;' width='150px'>Applicant Email:</td>
+ <td width='160px'>${ApplicantEmail}</td>
+</tr>
+<tr>
+ <td style='font-weight:600;vertical-align:text-top' width='130px'>Applicant State:</td>
+ <td width='160px'>${ApplicantState}</td>
+ <td width='50px'>
+ </td>
+ <td style='font-weight:600;vertical-align:bottom' width='150px'>Applicant Phone:</td>
+ <td style='vertical-align:bottom' width='160px'>${ApplicantTelephoneNumber}</td>
+</tr>
+</table>
